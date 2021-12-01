c17c094c9178fbbbab2ce303ddf4125ac0e015ef
Fran Diéguez <fran.dieguez@mabishu.com>
2021-12-01 12:11:36 +0100

    Update Layout.tsx

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -21,7 +21,6 @@ const Layout: React.FunctionComponent<Props> = ({
         name="description"
         content="Un lugar idílico donde descansar en el Camino Portugés de Santiago en Caldas de Reis."
       />      
-      <!-- Global site tag (gtag.js) - Google Analytics -->
       <script async src="https://www.googletagmanager.com/gtag/js?id=UA-213807336-1%22%3E"></script>
       <script>
         window.dataLayer = window.dataLayer || [];
